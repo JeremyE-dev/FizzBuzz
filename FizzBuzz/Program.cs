6dde187984c452b037c9ca4fae7bcf9c9d6876f6
Jeremy Eudaly <eudaly_19@hotmail.comeugit>
2020-02-09 09:56:03 -0600

    completed assignment

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("{0 } FizzBuzz", i);
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("{0 } Fizz", i);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("{0 } Buzz", i);
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
